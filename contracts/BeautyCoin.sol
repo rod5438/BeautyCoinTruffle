46f1753cc77e22a5542a61e4d0d926a4added448
Jason Wu <jason_wu@perfectCorp.com>
2018-03-28 14:19:13 +0800

    Remove unused function. Parent contract already implement.

--- contracts/BeautyCoin.sol
+++ contracts/BeautyCoin.sol
@@ -30,46 +30,7 @@ contract BeautyCoin is StandardToken {
         Transfer(publisher, msg.sender, totalBeautyCoin);
     }
 
-    function transfer(address _to, uint256 beautyCoin) public returns (bool) {
-        require(_to != address(0));
-        require(beautyCoin <= balances[msg.sender]);
-
-         // SafeMath.sub will throw if there is not enough balance.
-        balances[msg.sender] -= beautyCoin;
-        balances[_to] += beautyCoin;
-        Transfer(msg.sender, _to, beautyCoin);
-        return true;
-    }
-
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
-    }
-
     function balanceOfSelf() public view returns (uint256) {
         return balances[msg.sender];
     }
-
-    function transferFrom(address _from, address _to, uint256 beautyCoin) public returns (bool) {
-        require(_to != address(0));
-        require(beautyCoin <= balances[_from]);
-        require(beautyCoin <= allowed[_from][msg.sender]);
-        balances[_from] -= beautyCoin;
-        balances[_to] += beautyCoin;
-        allowed[_from][msg.sender] =- beautyCoin;
-        Transfer(_from, _to, beautyCoin);
-        return true;
-    }
-
-    function approve(address _spender, uint256 beautyCoin) public returns (bool) {
-        allowed[msg.sender][_spender] = beautyCoin;
-        Approval(msg.sender, _spender, beautyCoin);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) public view returns (uint256) {
-        return allowed[_owner][_spender];
-    }
-
-    event Transfer(address indexed from, address indexed to, uint256 beautyCoin);
-    event Approval(address indexed owner, address indexed spender, uint256 beautyCoin);
 }
